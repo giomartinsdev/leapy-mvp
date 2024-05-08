a6e61a096b5652fa720168b98c0cacab97713425
giovanni <giovannidealmeidamartins@gmail.com>
2024-05-08 13:09:21 -0300

    fix: fixing / api route

--- backend/src/routes/rootRouter.ts
+++ backend/src/routes/rootRouter.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", () => {
-  return "hi three"
+router.get("/", (req, res) => {
+  res.send("<h1>api url</h1>");
 });
 
 export default router;
